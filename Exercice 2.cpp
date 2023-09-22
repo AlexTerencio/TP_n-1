f487a0687485cf577d37064c7ca8d89706915025
AlexTerencio <145346170+AlexTerencio@users.noreply.github.com>
2023-09-22 17:10:03 +0200

    Exercice 2

---
+++ Exercice 2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;  //Ceci est commentaire sur une ligne
+int main()
+/* Ceci est un commentaire
+sur plusieurs lignes */
+{
+    std::cout << "Hello, World!" << endl;
+    return 0;
+}
